bdb71808d786fa78d619f3c00eca075c9969925d
Leonard Stronkhorst <l.stronkhorst@nipo.com>
2018-01-21 16:37:53 +0100

    add service worker

---
+++ sw.js
@@ -0,0 +1,33 @@
+var CACHE_NAME = 'poker-cache-v1';
+var urlsToCache = [
+  '/',
+  '/nipo-logo48.png',
+  '/nipo-logo96.png',
+  '/nipo-logo192.png',
+  '/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
